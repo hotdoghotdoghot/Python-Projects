2c5734fa6defa4a8280af33cc1c6b26093dc0608
Alex Ross <37475489+hotdoghotdoghot@users.noreply.github.com>
2019-01-11 12:37:53 -0500

    whoop whoop

--- Assign1-Cricket-ATR.py
+++ Assign1-Cricket-ATR.py
@@ -1,7 +1,7 @@
 #Assign1-Cricket-ATR
 #Alex Ross
 #This program calculates the temperature based on the frequency of cricket chirps
-
+#whoop whoop
 def main():
     chirps = eval(input("How many chirps did you hear in 60 seconds? "))
 
